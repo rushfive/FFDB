bfa3c31e836c92ec88e3f6f9f9ab515882f64ff6
Unknown <ilee@scisolutions.com>
2018-12-17 18:27:24 -0800

    prime num generator

---
+++ R5.FFDB.Components/PrimeGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R5.FFDB.Components
+{
+	public static class PrimeGenerator
+	{
+		public static int StartingAt(int start)
+		{
+			while (true)
+			{
+				if (IsPrime(start))
+				{
+					return start;
+				}
+
+				start++;
+			}
+		}
+
+		public static bool IsPrime(int number)
+		{
+			if ((number & 1) == 0)
+			{
+				return number == 2;
+			}
+
+			for (int i = 3; (i * i) <= number; i += 2)
+			{
+				if ((number % i) == 0)
+				{
+					return false;
+				}
+			}
+			return number != 1;
+		}
+	}
+}
